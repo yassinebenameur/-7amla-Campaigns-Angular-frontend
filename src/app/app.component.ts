ec34f544b9c1a1c73635496a55aeaa2199531f2f
Yassine ben ameur <superyassine15@gmail.com>
2020-01-12 18:44:34 +0100

    search template integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
 
 // declare var $: any;
 
@@ -9,29 +8,12 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  isCollapsed = false;
 
-  constructor(private router: Router) {
+  constructor() {
   }
 
-  onSearch() {
-    console.log('SEARCH');
-  }
-
-
-  onEnter(nzValue: any) {
-    console.log(nzValue);
-    this.router.navigateByUrl('search/' + nzValue);
-  }
 
   ngOnInit(): void {
-    // if ($('.main-header').attr('data-sticky_header')) {
-    //
-    //   $('.main-header .header-wrapper').waypoint('sticky', {
-    //     wrapper: '<div class="sticky-wrapper" />',
-    //     stuckClass: 'stuck'
-    //   });
-    //
-    // }
+
   }
 }
