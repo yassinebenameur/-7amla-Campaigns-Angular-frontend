bb46646c4fba59e7ad66b67edebbbe46e9ecea20
Katitoucherie <ayadi.malek@gmail.com>
2020-01-13 01:29:08 +0100

    users profil search

--- src/app/user/user-area/user-area.component.ts
+++ src/app/user/user-area/user-area.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../_services/authentication.service';
 import {AuthTokenModel} from '../../_models/auth-token.model';
 import {UserModel} from '../../_models/user.model';
+import {ActivatedRoute} from '@angular/router';
+import {Campaign} from '../../_models/campaign.model';
+import {CrudService} from '../../_services/crud.service';
+import {Globals} from '../../_globals/Globals';
 
 @Component({
   selector: 'app-user-area',
@@ -10,23 +14,41 @@ import {UserModel} from '../../_models/user.model';
 })
 export class UserAreaComponent implements OnInit {
 
-  display = -1 ;
-  user: AuthTokenModel;
+  display = -1;
+  currentUser: AuthTokenModel;
+  user: UserModel;
+  private userId: any;
+  private userUrl: string;
 
-  constructor(private authService: AuthenticationService) {
+  constructor(private authService: AuthenticationService, private  crudService: CrudService, private route: ActivatedRoute) {
+    this.userId = this.route.snapshot.params.id;
+    this.userUrl = Globals.API_URL + Globals.USER;
+
     this.display = 0;
     authService.refresh()
       .subscribe(() => {
         authService.currentUser
           .subscribe(
             x => {
-              this.user = x;
-              console.log(this.user);
+              this.currentUser = x;
             });
       });
   }
 
-  ngOnInit() {console.log(this.display);
+  ngOnInit() {
+    this.route.params.subscribe(routeParams => {
+      this.userId = routeParams['id'];
+    });
+
+    this.crudService.getOne<UserModel>(this.userUrl, this.userId)
+      .subscribe(user => {
+        if (this.userId === this.currentUser.user.id) {
+          this.user = this.currentUser.user;
+        } else {
+          this.user = user;
+        }
+        console.log(this.user);
+      });
   }
 
   changeDisplay(i: number) {
